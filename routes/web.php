6195319077c8502d971e553baeda1e16d39ece34
pollohultima <matteo.polonio@gmail.com>
2022-01-26 15:04:49 +0100

    Routing to Admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')->prefix('admin')->namespace('Admin')->name('admin.')->group(function(){
 
-
+    Route::get('/', 'HomeController@index')->name('index');
+});
