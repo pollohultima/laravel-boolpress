17a7a269b359161bf35d7c4ae81ed6eb5411747f
pollohultima <matteo.polonio@gmail.com>
2022-01-27 15:22:20 +0100

    Make category seeder, table and model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     protected $fillable = ['title', 'slug', 'sub_title', 'cover', 'body'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
---
+++ database/seeds/CategorySeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['Programming', 'Automation', 'Web design', 'Best Practices'];
+        foreach ($categories as $category) {
+            $cat = new Category();
+            $cat->name = $category;
+            $cat->slug = Str::slug($category);
+            $cat->save();
+        }
+    }
+}
