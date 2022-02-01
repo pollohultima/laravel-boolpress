beb57807d2c152ba402ef0ce74f77debc2055e97
pollohultima <matteo.polonio@gmail.com>
2022-02-01 17:38:44 +0100

    trying to get bonus 2

--- resources/views/admin/contacts/show.blade.php
+++ resources/views/admin/contacts/show.blade.php
@@ -11,5 +11,17 @@
         {{ $contact->message }}
     </p>
 
+    <hr>
+
+    <h2>Reply</h2>
+    <form action="{{ route('') }}" method="post">
+        @csrf
+        <div class="mb-3">
+            <label for="message" class="form-label">Message</label>
+            <textarea class="form-control" name="message" id="message" rows="5"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Send</button>
+    </form>
+
 
 @endsection
